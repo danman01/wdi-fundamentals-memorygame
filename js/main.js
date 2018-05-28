bfa7b72de2c02c0b9d4e723239a7901d215b15f8
cymbiotica <cymbioticasahon@gmail.com>
2018-05-28 09:58:10 -0400

    more game logic - unit 10

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
 console.log("Up and running!");
 
 
-var card = ['queen', 'queen', 'king', 'king'];
+var card = [{
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "/images/queen-of-hearts.png"
+    },
+
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "/images/queen-of-diamonds.png"
+    },
+
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "/images/king-of-hearts.png"
+    },
+
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "/images/king-of-diamonds.png"
+    }];
+
 var cardsInPlay = [];
 
-var cehckForMatch = function () {
+var checkForMatch = function () {
     if (cardsInPlay.length === 2) {
         if (cardsInPlay[0] === cardsInPlay[1]) {
             alert("You found a match");
@@ -15,11 +38,12 @@ var cehckForMatch = function () {
 };
 
 var flipCard = function (cardId) {
-    console.log("User Flipped " + card[cardId]);
-    cardsInPlay.push(card[cardId]);
-    cehckForMatch();
-    //console.log(cardsInPlay);
+    console.log("User Flipped " + card[cardId].rank);
+    cardsInPlay.push(card[cardId].rank);
+    checkForMatch();
+    console.log(card[cardId].suit);
+    console.log(card[cardId].cardImage);
 
 };
 flipCard(0);
-flipCard(1);
+flipCard(2);
